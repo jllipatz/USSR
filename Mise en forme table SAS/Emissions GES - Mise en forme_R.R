e4502070b7a46010e49c02cd2be66f5ec6f24e1c
NKempf <kempfnicolas@gmail.com>
2018-05-17 06:52:45 -0400

    Remove SAS table

--- Mise en forme table SAS/Emissions GES - Mise en forme_R.R
+++ Mise en forme table SAS/Emissions GES - Mise en forme_R.R
@@ -3,10 +3,22 @@
 #--------------------------------------------------------------------------------------------------------------------------------------------#
 
 #----------------------------#
-#   I. Import de la table    #
+#   I. Import des bases      #
 #----------------------------#
+
+# Chargement de la base 
 schema <- read.table(file = 'Mise en forme table SAS/Fichdep/env_ac_ainah_r2.tsv', sep = '\t', header = TRUE,stringsAsFactors = FALSE)
 str(schema)
 
+# Mise à NA des ": "
 schema[schema == ": "] <- NA
-schema
+
+# Changement des noms de colonnes
+test <- 2:length(colnames(schema))
+colnames(schema) <- c("champ",paste("A",substr(colnames(schema)[test],2,length(colnames(schema)[test])),sep=""))
+
+# Les caractères spéciaux et lettres présents au milieu des données numériques sont supprimés 
+# (ex : b=rupture de série, c=confidentiel, d=définition différente, voir métadonnées, e=estimé, f=prévision, i=voir métadonnées (bientôt supprimé), 
+# n=non significatif, p=provisoire, r=révisé, s=estimation Eurostat (bientôt supprimé), u =peu fiable, z=non applicable, :=valeur manquante)
+# "benscfpudirz:"
+schema[,2:ncol(schema)] <- gsub("b|e|n|s|c|f|p|u|d|i|r|z", "", schema[,2:ncol(schema)])
